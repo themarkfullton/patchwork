e481c0015aacb58a4e8472c02a9ee21068f7b02e
Mark <the.mark.fullton@gmail.com>
2020-10-09 15:37:20 -0700

    Added basic Patch controller

--- src/main/java/com/tmf/Controller/PatchController.java
+++ src/main/java/com/tmf/Controller/PatchController.java
@@ -1,4 +1,16 @@
 package com.tmf.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/patch")
 public class PatchController {
+
+    @GetMapping
+    public String getPatches() {
+        return "Hello World";
+    }
 }
