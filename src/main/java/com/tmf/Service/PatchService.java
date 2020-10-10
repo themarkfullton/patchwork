7208a96fab276e3b84a72a91f7cff86756b56ceb
Mark <the.mark.fullton@gmail.com>
2020-10-09 22:02:01 -0700

    Added get/create Patch to PatchService

--- src/main/java/com/tmf/Service/PatchService.java
+++ src/main/java/com/tmf/Service/PatchService.java
@@ -1,12 +1,24 @@
 package com.tmf.Service;
 
+import com.tmf.DAO.PatchDAO;
+import com.tmf.Models.Patch;
+import com.tmf.Models.PatchUpdatePayload;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+import java.util.Collection;
+import java.util.Optional;
 
 @Service
 public class PatchService {
-    public Collection<Patches> getPatches() {
+    @Autowired
+    private PatchDAO patchDAO;
 
+    public Collection<Patch> getPatches() {
+        return patchDAO.getPatches();
+    }
+
+    public Patch createPatch(Patch patch) {
+        return patchDAO.createPatch(patch);
     }
 }
