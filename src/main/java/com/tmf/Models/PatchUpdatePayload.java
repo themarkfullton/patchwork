eabebe11e846b3b800593213e632cd364487078d
Mark <the.mark.fullton@gmail.com>
2020-10-09 21:47:28 -0700

    Added patch Update model

---
+++ src/main/java/com/tmf/Models/PatchUpdatePayload.java
@@ -0,0 +1,25 @@
+package com.tmf.Models;
+
+public class PatchUpdatePayload {
+    private Integer owner;
+    private String type;
+    private String pattern;
+    private Integer level;
+    private Integer stage;
+    private String personality;
+    private Integer hunger;
+    private Integer happiness;
+    private Boolean isHealthy;
+    private Integer strength;
+    private Integer defense;
+    private Integer agility;
+    private Integer hp;
+    private Integer wp;
+    private Integer ptVoid;
+    private Integer ptBlood;
+    private Integer ptBlight;
+    private Integer ptSpirit;
+    private Integer ptWill;
+    private Integer ptEnigma;
+    private Integer ptPassion;
+}
