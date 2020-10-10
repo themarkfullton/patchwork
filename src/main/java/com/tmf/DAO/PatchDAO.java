15fe542afbd9de578765005fd75192c3094b4c63
Mark <the.mark.fullton@gmail.com>
2020-10-09 21:44:49 -0700

    added DAO folder; adding PatchDAO

---
+++ src/main/java/com/tmf/DAO/PatchDAO.java
@@ -0,0 +1,13 @@
+package com.tmf.DAO;
+
+import com.tmf.Models.Patch;
+import com.tmf.Models.PatchUpdatePayload;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Component
+public class PatchDAO {
+}
--- src/main/java/com/tmf/Service/PatchService.java
+++ src/main/java/com/tmf/Service/PatchService.java
@@ -2,6 +2,8 @@ package com.tmf.Service;
 
 import org.springframework.stereotype.Service;
 
+
+
 @Service
 public class PatchService {
     public Collection<Patches> getPatches() {
