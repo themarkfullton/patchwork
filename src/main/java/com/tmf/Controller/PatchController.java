907d97e0ab0041c1098b4da6aee0b5e7c0444065
Mark <the.mark.fullton@gmail.com>
2020-10-09 22:14:04 -0700

    Added models and service to patch controller

--- src/main/java/com/tmf/Controller/PatchController.java
+++ src/main/java/com/tmf/Controller/PatchController.java
@@ -1,16 +1,19 @@
 package com.tmf.Controller;
 
+import com.tmf.Models.Patch;
+import com.tmf.Models.PatchUpdatePayload;
+import com.tmf.Service.PatchService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/patch")
 public class PatchController {
+    @Autowired
+    private PatchService patchService;
 
-    @GetMapping
-    public String getPatches() {
-        return "Hello World";
-    }
+
 }
